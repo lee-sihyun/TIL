2f2bb92c5edafdcc1831c2bd2f7bfbb81bb6ad40
shyun <shyun030926@gmail.com>
2021-03-10 09:02:33 +0900

    jvm메모리모델,백준 1001

--- src/coding/Cal.java
+++ src/coding/Cal.java
@@ -1,20 +1,26 @@
 package coding;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Cal {
 
 	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
 
-		Scanner scanner = new Scanner(System.in);
+		int c = sc.nextInt();
+		int d = sc.nextInt();
 
-		try {
-			int a = scanner.nextInt();
-			int b = scanner.nextInt();
-			System.out.println(a + b);
-		} catch (Exception e) {
-			System.out.printf("에러", e);
-		}
+
+		/*
+		 * try { int a = scanner.nextInt(); int b = scanner.nextInt();
+		 * System.out.println(a + b); } catch (Exception e) { System.out.printf("에러",
+		 * e); }
+		 */
+
+
+		System.out.println(c-d);
 
 	}
+
 }
