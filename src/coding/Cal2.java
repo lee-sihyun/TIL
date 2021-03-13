40eca7ee0142c7eb2eed82343b9e272bb28c1abf
shyun <shyun030926@gmail.com>
2021-03-14 08:04:54 +0900

    스택,큐,덱, 백준 10430

---
+++ src/coding/Cal2.java
@@ -0,0 +1,26 @@
+package coding;
+
+import java.util.*;
+
+public class Cal2 {
+	public static void main(String[] args) {
+
+		/* 10430 */
+
+		Scanner sc = new Scanner(System.in);
+
+		int a, b, c;
+
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+
+		System.out.println((a + b) % c);
+		System.out.println((a % c + b % c) % c);
+		
+		System.out.println((a * b) % c);
+		System.out.println((a % c + b % c) % c);
+
+
+	}
+}
