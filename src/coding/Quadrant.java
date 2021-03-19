badeb495616340524f8bc763f89798ade7af44fb
shyun <shyun030926@gmail.com>
2021-03-19 09:09:18 +0900

    백준 14681

---
+++ src/coding/Quadrant.java
@@ -0,0 +1,26 @@
+package coding;
+
+import java.util.*;
+
+public class Quadrant {
+/*14681*/
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int x = in.nextInt();
+		int y = in.nextInt();
+		if (x > 0) {
+			if (y > 0) {
+				System.out.print(1);
+			} else {
+				System.out.print(4);
+			}
+		} else {
+			if (y > 0) {
+				System.out.print(2);
+			} else {
+				System.out.print(3);
+			}
+		}
+	}
+}
