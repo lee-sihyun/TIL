29c67d03506cda828a41d6430cc7f4042bbff5cb
shyun <shyun030926@gmail.com>
2021-03-22 09:16:34 +0900

    백준 10950

---
+++ src/coding/Cal3.java
@@ -0,0 +1,20 @@
+package coding;
+
+import java.util.*;
+
+public class Cal3 {
+	public static void main(String[] args) {
+
+		/* 10950 */
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a + b);
+		}
+
+	}
+}
