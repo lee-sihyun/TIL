e9926c4d382a65048d1cb001268fb68814de54bd
shyun <shyun030926@gmail.com>
2021-03-23 11:15:41 +0900

    백준 8393

---
+++ src/coding/Sum.java
@@ -0,0 +1,22 @@
+package coding;
+
+import java.util.*;
+
+public class Sum {
+	public static void main(String[] args) {
+
+		/* 8393 */
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+
+		for (int i = 1; i <= n; i++) {
+
+			sum += i;
+
+		}
+
+		System.out.println(sum);
+	}
+}
