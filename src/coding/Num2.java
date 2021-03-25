00ccdf9f71a71e3dd83475e8ed382cc9e827e7f7
shyun <shyun030926@gmail.com>
2021-03-26 08:57:05 +0900

    백준 2742

--- src/coding/Num2.java
+++ src/coding/Num2.java
@@ -3,23 +3,16 @@ package coding;
 import java.util.*;
 
 public class Num2 {
-public static void main(String[] args) {
-	
-	
-	
-	/*2742 */
-	
-	
-	
-	Scanner sc= new Scanner(System.in);
-	int n= sc.nextInt();
-	
-	for(int i=n; i>0; i--) {
-		System.out.println(i);
+	public static void main(String[] args) {
+
+		/* 2742 */
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = n; i > 0; i--) {
+			System.out.println(i);
+		}
+
 	}
-	
-	
-	
-	
-}
 }
