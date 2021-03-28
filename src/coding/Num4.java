7cfe539e682a19bc1da2bf1287f39f46bb80ffc4
shyun <shyun030926@gmail.com>
2021-03-28 11:08:57 +0900

    백준 11022

---
+++ src/coding/Num4.java
@@ -0,0 +1,26 @@
+package coding;
+
+import java.util.*;
+
+public class Num4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/* 11022 */
+
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		for (int i = 1; i <= T; i++) {
+
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+
+			System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a + b));
+		}
+
+	}
+
+}
