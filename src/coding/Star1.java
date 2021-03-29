90054c77a0154f2ee469588c27b3c09b4a45af35
shyun <shyun030926@gmail.com>
2021-03-29 09:46:45 +0900

    백준 2438

---
+++ src/coding/Star1.java
@@ -0,0 +1,23 @@
+package coding;
+
+import java.util.*;
+
+public class Star1 {
+	public static void main(String[] args) {
+
+		/* 2438 */
+
+		Scanner sc = new Scanner(System.in);
+
+		int s = sc.nextInt();
+
+		for (int i = 0; i <= s; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+
+	}
+}
