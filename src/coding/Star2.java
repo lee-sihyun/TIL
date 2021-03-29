bacbe0f5a32d476e87bcff6134e46cdbb793bb79
shyun <shyun030926@gmail.com>
2021-03-30 08:10:29 +0900

    백준 2439

---
+++ src/coding/Star2.java
@@ -0,0 +1,27 @@
+package coding;
+
+import java.util.*;
+
+public class Star2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+
+			for (int k = 1; k <= i; ++k) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
