2b6dab0fa0a5aef5e0e0e51e64d8f243fee6b98d
shyun <shyun030926@gmail.com>
2021-04-01 11:26:53 +0900

    백준 10952

---
+++ src/coding/While1.java
@@ -0,0 +1,21 @@
+package coding;
+
+import java.util.*;
+
+public class While1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+
+			if (A == 0 && B == 0) {
+
+				break;
+			}
+			System.out.println(A + B);
+		}
+	}
+}
